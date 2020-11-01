731d2727a103f96df8070f2f8c62881c2211f907
wwwwwmw <william.manwai@gmail.com>
2020-11-01 23:23:12 +0800

    layout frontpage completed

--- src/Row.js
+++ src/Row.js
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from 'react'
 import './Row.css'
 import instance from './axios'
+import Youtube from 'react-youtube'
+import movieTrailer from 'movie-trailer'
 
 const base_url = 'http://image.tmdb.org/t/p/original/'
 
 function Row({title, fetchUrl, isLargeRow}) {
   const [movies, setMovies] = useState([])
+  const [trailerUrl, setTrailerUrl] = useState("")
 
   useEffect(()=> {
     async function fetchData() {
@@ -16,19 +19,48 @@ function Row({title, fetchUrl, isLargeRow}) {
     }
     fetchData()
   },[fetchUrl])
-   console.log(movies)
+  
+  const opts = {
+    heifht : '390',
+    width:'100%',
+    playerVars:{
+      autoplay:1,
+    }
+  }
+  const handleClick = (movie) => {
+    console.log('entry', movie)
+    if(trailerUrl) {
+      setTrailerUrl('')
+    }else{
+      const ans = movie?.name || movie?.original_name ||  movie?.original_title       
+      console.log('ansssss', ans)
+      movieTrailer(ans ||  "")
+      .then(url => {
+        // https://www.youtube.com/watch?v=XtMThy8QKqU&t=3443s
+        console.log('urlurl',url)
+        const urlParms = new URLSearchParams( new URL(url).search)
+        // console.log('vvvvv', urlParms.get('v'))
+        setTrailerUrl(urlParms.get('v'))
+      })
+      .catch((err) => console.log(err))
+    }
+  }
+
   return (
     <div className="row" >
       <h2>{title}</h2>
       <div className="row__posters">
         {movies.map(movie => (
           <img 
+            onClick={() => handleClick(movie)}
             className={`row__poster ${isLargeRow && "row__posterLager"} `}
             key={movie.id}
             src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} 
             alt={movie.original_title} />
           ))}
       </div>
+      {trailerUrl  &&  <Youtube videoId={trailerUrl} opts={opts} />}
+      {/* <Youtube videoId={"wKfQ0GVFqCQ"} opts={opts} /> */}
     </div>
   )
 }
